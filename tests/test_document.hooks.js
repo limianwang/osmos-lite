1a2814dda71d4e149384e62d2bbfe01731cf5617
Marco Tabini <marcot@tabini.ca>
2013-06-19 14:20:23 -0400

    - Fix error reporting in document hook chain

--- tests/test_document.hooks.js
+++ tests/test_document.hooks.js
@@ -55,4 +55,42 @@ describe('The document class hook system', function() {
             })
         });
     });
+
+    it('should allow attaching to the didValidate hook', function(done) {
+        function plugin(doc) {
+            doc.hook('didValidate', function(data, callback) {
+                expect(data.document).to.be.an('object');
+                expect(data.document.constructor.name).to.equal('OsmosDocument');
+                
+                expect(data.errors).to.be.an('array');
+                expect(data.errors).to.have.length(0);
+                
+                data.errors.push(new Error('Invalid astroturfing bit.'));
+                
+                callback(null);
+            });
+        };
+        
+        model.create(function(err, doc) {
+            doc.plugin(plugin);
+            
+            doc.name = 'Marco';
+            
+            doc.save(function(errs) {
+                expect(errs).to.be.an('array');
+                expect(errs).to.have.length(1);
+                
+                var err = errs[0];
+                
+                expect(err).to.be.an('object');
+                expect(err.constructor.name).to.equal('OsmosError');
+                expect(err.message).to.equal('Invalid astroturfing bit.');
+                
+                expect(doc._primaryKey).to.be.undefined;
+                
+                done();
+            })
+        });
+    });
+    
 });
